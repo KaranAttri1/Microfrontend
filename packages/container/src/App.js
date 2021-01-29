351126500467abe3ff71ec38308a62660030c5a3
Karan Attri <karan.attri@KaranAttriMac.local>
2021-01-30 02:10:13 +0530

    conta

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp'
 export default ()=>{
     return(
         <div>
-            <h1>Marketing Bois</h1>
+            <h1>Marketing Boisz</h1>
             <hr/>
             <MarketingApp/>
         </div>
