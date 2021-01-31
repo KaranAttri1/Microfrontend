5f8f96ba25309cc23817d58d2d968d9af1b3f00d
Karan Attri <karan.attri@KaranAttriMac.local>
2021-01-31 23:25:59 +0530

    assss

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,15 +2,19 @@ import React from 'react'
 import {BrowserRouter} from 'react-router-dom'
 import MarketingApp from './components/MarketingApp'
 import Header from './components/Header'
-
+import {StylesProvider,createGenerateClassName} from '@material-ui/core/styles'
+const generateClassName=createGenerateClassName({
+    productionPrefix:'con'
+})
 export default ()=>{
     return(
+        <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
         <div>
             <Header/>
             <MarketingApp/>
         </div>
         </BrowserRouter>
-
+        </StylesProvider>
     )
 }
