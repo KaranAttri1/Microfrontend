fc5358f0f88fa832779475d7dd03421bd8c87dca
Karan Attri <karan.attri@KaranAttriMac.local>
2021-02-04 23:16:15 +0530

    market to cont

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -3,12 +3,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import {createMemoryHistory} from 'history'
-const mount = (el) =>{
+import {useHistory} from 'react-router-dom'
+
+const mount = (el,{onNavigate}) =>{
     const history=createMemoryHistory()
+    if(onNavigate){
+    history.listen(onNavigate)
+    }
 ReactDOM.render(
-    <App history={history}/>,
-    el
-)
+    <App history={history}/>,el)
 }
 if(process.env.NODE_ENV=='development'){
     const devRoot=document.querySelector('#marketing-dev-root')
